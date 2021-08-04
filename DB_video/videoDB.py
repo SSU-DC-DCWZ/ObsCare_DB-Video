046ce3a3cf2177c2261858f2ebf91d7fb67c36f1
ChanSeo <lcs0713v@gmail.com>
2021-08-04 18:09:09 +0900

    DB에 대해 단일 동작하도록 DBvideo class 수정+주석 수정

--- DB_video/videoDB.py
+++ DB_video/videoDB.py
@@ -40,25 +40,3 @@ class DBvideo:
     def makerecord(self):
         self.conn.execute('INSERT INTO video_' + self.camnum + ' VALUES (?,?)', self.now.strftime('%d'), self.path)
         self.conn.commit()
-
-
-    # def __init__(self, camnum, now):
-    #     savename = "./" + now.strftime('%Y%m') + ".db"
-    #     try:  # 파일 경로 생성, 경로가 존재 하지 않을 경우 파일 경로 생성
-    #         if not (os.path.isdir(savename)):
-    #             os.makedirs(os.path.join(savename))
-    #     except OSError as e:  # 생성 실패 시 오류 코드 출력
-    #         if e.errno != errno.EEXIST:
-    #             print("Dir error")
-    #     raise
-    #
-    #     self.conn = sqlite3.connect(savename)
-    #     self.cur = self.conn.cursor()
-    #     self.cur.execute('CREATE TABLE video_data(day INTEGER, video_address TEXT)')
-    #     self.con.close()
-
-    #
-    # def adddb(self):
-    #     self.cur.execute("INSERT INTO video_data(now.strftime('%d'), './recording/' + str(camnum) + now.strftime('%Y%m%d')")
-    #     self.conn.commit()
-    #     self.conn.close()
