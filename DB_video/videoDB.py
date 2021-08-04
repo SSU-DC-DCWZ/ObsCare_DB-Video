1a6ab14771f94cef215b8a29a4bfb09e47d072bf
ChanSeo <lcs0713v@gmail.com>
2021-08-04 18:07:55 +0900

    DB에 대해 단일 동작하도록 DBvideo class 수정

--- DB_video/videoDB.py
+++ DB_video/videoDB.py
@@ -1,39 +1,46 @@
 import sqlite3
 import datetime
 import os.path
-import errno
 
 class DBvideo:
-    def __init__(self, camnum,time):
+    def __init__(self, num, time, path): #num=DB 작업할 카메라 번호, time=datetime.datetime.now(), path=동영상의 절대 경로
         self.now = time
+        self.camnum = num
+        self.path = path
         self.dbname = "./DB_video/DB/" + self.now.strftime('%Y%m') + ".db"
-        try:  # 파일 경로 생성, DB파일이 존재 하지 않을 경우 DB파일 생성
-            if not (os.path.isfile(self.dbname)):
-                self.conn = sqlite3.connect(self.dbname)
-                self.makedb(camnum)
-        except OSError as e:  # 생성 실패 시 오류 코드 출력
-            if e.errno != errno.EEXIST:
-                print("Dir error")
-        raise
-        self.conn = sqlite3.connect(dbname)
+        self.makedb()
         self.cur = self.conn.cursor()
         self.conn.commit()
 
     def closedb(self):
         self.conn.close()
 
-    def makedb(self, camnum):
-        for i in range(0,camnum):
-            self.cur.execute('CREATE TABLE video_' + i + '(id INTEGER, video_address TEXT)')
+    def makedb(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cur.execute('CREATE TABLE video_' + self.camnum + ' (id INTEGER, video_address TEXT)')
         self.conn.commit()
 
+    def delrecord(self):
+        if self.cur.rowcount == 1:
+            self.deldb()
+        else:
+            self.conn.execute('DELETE FROM video_' + self.camnum + ' WHERE id is' + self.now.strftime('%d'))
+            self.conn.commit()
+
     def deldb(self):
+        self.closedb()
+        if os.path.isfile(self.dbname):
+            os.remove(self.dbname)
 
     def findrecord(self, findnum):
+        self.cur.execute('SELECT * FROM viedo_' + findnum + ' WHERE id=' + self.now.strftime('%d'))
+        path = self.cur.fetchone()
+        return path
 
     def makerecord(self):
+        self.conn.execute('INSERT INTO video_' + self.camnum + ' VALUES (?,?)', self.now.strftime('%d'), self.path)
+        self.conn.commit()
 
-    def delrecord(self):
 
     # def __init__(self, camnum, now):
     #     savename = "./" + now.strftime('%Y%m') + ".db"
