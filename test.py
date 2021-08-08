246863a7ecc4e4d6cc5a11b37ce6ba63d6f2ff0a
ChanSeo <lcs0713v@gmail.com>
2021-08-09 04:19:26 +0900

    .gitignore 수정

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 from Stream import stream
+from DB_video import videoDB
 import threading
+import datetime
 
 r = stream.Stream(0)
-th = threading.Thread(target=r.start)
-th.start()
+th1 = threading.Thread(target=r.start)
+th1.start()
+day = datetime.datetime(2021, 8, 8, 00, 00, 00)
+d = videoDB.DBvideo(0, day)
+a = d.findrecord()
+del a
+r.sign = 1
+
