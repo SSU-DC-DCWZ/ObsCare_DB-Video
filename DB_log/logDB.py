294cdbd766b8a0ed5f016a377b245fa12abd06f9
ChanSeo <lcs0713v@gmail.com>
2021-08-09 04:06:00 +0900

    log에 대한 DB처리 기능 제작 및 주석 추가

--- DB_log/logDB.py
+++ DB_log/logDB.py
@@ -1,15 +1,22 @@
 import sqlite3
 
+#log 발생에 대한 DB 처리 클래스
 class DBlog:
-    def __init__(self, situation):
+    def __init__(self, situation): #situation:발생상황
         self.situation = situation
-        self.conn = sqlite3.connect('./db/log.db')
-        self.cur = self.conn.cursor()
         self.connectdb()
 
     def __del__(self):
         pass
 
-    def connectdb(self):
-        self.cur.execute("CREATE TABLE IF NOT EXISTS log_" + str(self.situation) + "  (day INTEGER, screen_address TEXT, image BLOB )")
+    def connectdb(self): #DB파일 선언 및 테이블 없을 경우 테이블 생성하는 함수
+        self.conn = sqlite3.connect('./db/log.db')
+        self.cur = self.conn.cursor()
+        #카메라 별로 별도의 테이블 생성
+        self.cur.execute("CREATE TABLE IF NOT EXISTS log_" + str(self.situation) +
+                         " (day INTEGER, camera INTEGER, screenshot_address TEXT, image BLOB )")
         self.conn.commit()
+
+    def closedb(self): #DB 종료 함수
+        self.conn.commit()
+        self.conn.close()
