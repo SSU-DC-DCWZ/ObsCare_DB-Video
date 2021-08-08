0226cce0dd14cd3b1971738d8bf4007ddeeeedc6
ChanSeo <lcs0713v@gmail.com>
2021-08-09 04:06:29 +0900

    클래스 및 클래스 내의 함수에 대한 주석 추가

--- Stream/stream.py
+++ Stream/stream.py
@@ -7,25 +7,22 @@ from DB_video import videoDB
 class Stream: #cctv 스트리밍을 위한 클래스 설계
     def __init__(self, camnum):
         self.running = True
-        self.camnum = camnum
-        self.sign = 0
+        self.camnum = camnum    #카메라 번호, cctv 시스템에서는 카메라 위치 번호
+        self.sign = 0   #스크린샷이 필요한 상황 발생 시 상황별 번호(0:상황 없음, 1:환자발생, 1~4:도움이 필요한사람)
 
-    def run(self):
-        print("start")
+    def run(self): #스트리밍을 시작하기 위한 함수
         while self.running:
             self.video()
             now = datetime.datetime.now()
-            if now.strftime('%H%M%S') == '000000':
+            if now.strftime('%H%M%S') == '000000': #일단위 저장을 위해 00시 00분 00초가 되면 스트리밍을 멈추고 재시작
                 self.stop()
                 self.start()
 
-    def video(self):
+    def video(self): #영상에 대한 처리를 위함 함수
         ret, frame = self.capture.read()
         cv2.imshow("VideoFrame1", frame)
         self.out.write(frame)
-        k = cv2.waitKey(1)
-        if self.sign >> 0:
-            print("screenshot")
+        if self.sign >> 0: #상황 발생 시 스크린샷을 위한 처리
             now = datetime.datetime.now()
             name = './data/Situation/' + str(self.sign) + '/' + now.strftime('%Y%m%d_%H_%M_%S') +'.jpg'
             try:  # 파일 경로 생성, 경로가 존재 하지 않을 경우 파일 경로 생성
@@ -37,18 +34,18 @@ class Stream: #cctv 스트리밍을 위한 클래스 설계
                 raise
             cv2.imwrite(name, frame)
             self.sign = 0
-
-        if k == 27:
+        # 1ms 동안 키입력 대기 ESC키 눌리면 종료
+        if cv2.waitKey(1) == 27:
             self.stop()
 
-
-    def stop(self):
+    def stop(self): #스트리밍 정지 및 저장, DB에 파일 저장
         self.running = False
         self.capture.release()
-        DB = videoDB.DBvideo(self.camnum, self.now, self.savename)
-        DB.makerecord()
+        db = videoDB.DBvideo(self.camnum, self.now, self.savename)
+        db.makerecord()
+        del db
 
-    def start(self):
+    def start(self): #스트리밍 시작 설정
         self.running = True
         self.capture = cv2.VideoCapture(self.camnum, cv2.CAP_DSHOW)
         self.width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
