20110da3663eda352e9496c6cca6fc9c31f5af35
ChanSeo <lcs0713v@gmail.com>
2021-08-11 17:03:09 +0900

    주석 수정

--- DB_log/logDB.py
+++ DB_log/logDB.py
@@ -17,9 +17,6 @@ class DBlog:
     def connectdb(self): #DB파일 선언 및 테이블 없을 경우 테이블 생성하는 함수
         self.conn = sqlite3.connect('./db/log.db')
         self.cur = self.conn.cursor()
-        #카메라 별로 별도의 테이블 생성
-        # self.cur.execute("CREATE TABLE IF NOT EXISTS log_" + str(self.situation) +
-        #                  " (day INTEGER, camera INTEGER, screenshot_address TEXT, image BLOB )")
         self.cur.execute("CREATE TABLE IF NOT EXISTS log_" + str(self.situation) +
                                            " (day INTEGER, camera INTEGER, screenshot_address TEXT)")
         self.conn.commit()
