0debe11eec1bcc4e0c76eb86f7927f4d06c5aab9
ChanSeo <lcs0713v@gmail.com>
2021-08-12 20:17:48 +0900

    저장기한 만료 레코드 삭제 기능 구현

--- DB_log/logDB.py
+++ DB_log/logDB.py
@@ -5,14 +5,14 @@ import os
 
 #log 발생에 대한 DB 처리 클래스
 class DBlog:
-    def __init__(self, situation, time, path): #situation:발생상황
+    def __init__(self, situation, time, path): #situation:발생상황, time=그떄의 시간, path:스크린샷 저장 경로
         self.situation = situation
         self.now = time
         self.path = path
         self.connectdb()
 
     def __del__(self):
-        pass
+        self.closedb()  # 레코드 생성할 때는 일자가 바뀌었다는 뜻이므로 동시에 레코드 삭제 수행
 
     def connectdb(self): #DB파일 선언 및 테이블 없을 경우 테이블 생성하는 함수
         self.conn = sqlite3.connect('./db/log.db')
@@ -21,7 +21,8 @@ class DBlog:
                                            " (day INTEGER, camera INTEGER, screenshot_address TEXT)")
         self.conn.commit()
 
-    def closedb(self): #DB 종료 함수
+    def closedb(self): #DB 종료 함수, 종료 시 저장기한 확인 수행
+        self.delrecord()
         self.conn.commit()
         self.conn.close()
 
@@ -30,17 +31,20 @@ class DBlog:
                       (self.now.strftime('%Y%m%d%H%M%S'), camnum, self.path))
 
         self.conn.commit()
-        self.delrecord() # 레코드 생성할 때는 일자가 바뀌었다는 뜻이므로 동시에 레코드 삭제 수행
-        self.conn.close()
 
-    def delrecord(self): # 저장기한 만료된 영상에 대한 DB 처리 및 삭제 함수
+    def delrecord(self): # 저장기한 만료된 스크린샷에 대한 DB 처리 및 삭제 함수
         lastday = self.now - datetime.timedelta(weeks=10) #저장기한 10주로 설정
-        self.cur.execute("SELECT * FROM log_" + str(self.situation) + " WHERE day=" + lastday.strftime('%Y%m%d%H%M%S'))
-        try:
-            path = self.cur.fetchone()[1]
-        except TypeError:  # path가 없을 경우 빈 문자열 반환(상위 레벨에서 비어있을 경우에 해당하는 처리 필요)
-            path = ''
-        if os.path.isfile(path):
-            os.remove(path)
-        self.conn.execute("DELETE FROM log_" + str(self.situation) + " WHERE day=" + lastday.strftime('%Y%m%d%H%M%S'))
+        while True: #삭제 일자에 해당하는 레코드 탐색
+            delpic = lastday.strftime('%Y%m%d') + '%'
+            self.cur.execute("SELECT * FROM log_" + str(self.situation) + " WHERE day LIKE '%s'"%str(delpic))
+            list = self.cur.fetchone()
+            try:
+                del_time = list[0]
+                path = list[2]
+            except TypeError:  # path가 없을 경우 더이상 삭제할 스크린샷이 없다 판단하여 반복문 탈출
+                break
+            if os.path.isfile(path):
+                os.remove(path)
+            self.conn.execute("DELETE FROM log_" + str(self.situation) + " WHERE day = " + str(del_time))
+            self.conn.commit()
         self.conn.commit()
