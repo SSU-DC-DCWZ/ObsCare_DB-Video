101f8242065ea5c9000889f6025b217b59fde9ed
ChanSeo <lcs0713v@gmail.com>
2021-08-17 13:30:48 +0900

    Stream class 세부사항 수정

--- Stream/stream.py
+++ Stream/stream.py
@@ -14,6 +14,31 @@ class Stream: #cctv 스트리밍을 위한 클래스 설계
         self.camnum = camnum    #카메라 번호, cctv 시스템에서는 카메라 위치 번호
         self.sign = 0   #스크린샷이 필요한 상황 발생 시 상황별 번호(0:상황 없음, 1:환자발생, 1~4:도움이 필요한사람)
 
+    def stop(self):  # 스트리밍 정지 및 저장, DB에 파일 저장
+        self.running = False
+        self.capture.release()
+        db = videoDB.DBvideo(self.camnum, self.starttime, self.savename)
+        db.makerecord()
+        del db
+
+    def start(self):  # 스트리밍 시작 설정
+        self.running = True
+        self.capture = cv2.VideoCapture(self.camnum, cv2.CAP_DSHOW)
+        self.width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.starttime = datetime.datetime.now()
+        self.savename = "./data/Recording/" + str(self.camnum) + "/" + self.starttime.strftime('%Y%m%d') + ".mp4"
+        try:  # 파일 경로 생성, 경로가 존재 하지 않을 경우 파일 경로 생성
+            if not (os.path.isdir("./data/Recording/" + str(self.camnum))):
+                os.makedirs(os.path.join("./data/Recording/" + str(self.camnum)))
+        except OSError as e:  # 생성 실패 시 오류 코드 출력
+            if e.errno != errno.EEXIST:
+                print("Dir error")
+            raise
+        codec = cv2.VideoWriter_fourcc(*'mp4v')
+        self.out = cv2.VideoWriter(self.savename, codec, 20.0, ((int(self.width)), (int(self.height))))
+        self.run()
+
     def run(self): #스트리밍을 시작하기 위한 함수
         while self.running:
             self.video()
@@ -49,28 +74,3 @@ class Stream: #cctv 스트리밍을 위한 클래스 설계
         # 1ms 동안 키입력 대기 ESC키 눌리면 종료
         if cv2.waitKey(1) == 27:
             self.stop()
-
-    def stop(self): #스트리밍 정지 및 저장, DB에 파일 저장
-        self.running = False
-        self.capture.release()
-        db = videoDB.DBvideo(self.camnum, self.now, self.savename)
-        db.makerecord()
-        del db
-
-    def start(self): #스트리밍 시작 설정
-        self.running = True
-        self.capture = cv2.VideoCapture(self.camnum, cv2.CAP_DSHOW)
-        self.width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.now = datetime.datetime.now()
-        self.savename = "./data/Recording/" + str(self.camnum) + "/" + self.now.strftime('%Y%m%d') + ".mp4"
-        try:  # 파일 경로 생성, 경로가 존재 하지 않을 경우 파일 경로 생성
-            if not (os.path.isdir("./data/Recording/" + str(self.camnum))):
-                os.makedirs(os.path.join("./data/Recording/" + str(self.camnum)))
-        except OSError as e:  # 생성 실패 시 오류 코드 출력
-            if e.errno != errno.EEXIST:
-                print("Dir error")
-            raise
-        codec = cv2.VideoWriter_fourcc(*'mp4v')
-        self.out = cv2.VideoWriter(self.savename, codec, 20.0, ((int(self.width)), (int(self.height))))
-        self.run()
